5c24fe6264beb4f82162c7e7d81ddd956ae05e1f
Kaddy2b <gabriel2b20600@hotmail.fr>
2017-02-12 22:24:41 +0100

    starfoulah j'avais oublié

--- Web_js/td01/zoom/script.js
+++ Web_js/td01/zoom/script.js
@@ -1,17 +1,13 @@
-function Zoom () {
-	var img1 = document.getElementById('image1');
-	var img2 = document.getElementById('image2');
-	var img3 = document.getElementById('image3');
-	var img4 = document.getElementById('image4');
-	var img5 = document.getElementById('image5');
-
-	img1.onmouseover = zoom1;
-	img2.onmouseover = zoom2;
-	img3.onmouseover = zoom3;
-	img4.onmouseover = zoom4;
-	img5.onmouseover = zoom5;
+function imgHighR(x) {
+	x.style.height += "20%";
+	x.style.width += "20%";
+	x.style.opacity = "1.0";
+	x.style.paddingLeft = "70px";
 }
 
-function zoom1 (image) {
-	// body...
+function imgLowR(x) {
+	x.style.height = "";
+	x.style.width = "";
+	x.style.opacity = "0.2";
+	x.style.paddingLeft = "50px";
 }
